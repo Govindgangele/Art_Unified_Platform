ed363e62a916cd5e24a666d6ff0245969d781bbc
govind <govindgangele123@gmail.com>
2026-08-24 19:44:11 +0530

    debugging 12

--- Backend/services/aiService.js
+++ Backend/services/aiService.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const AI_SERVICE_URL =
   process.env.AI_SERVICE_URL || "http://localhost:8000";
-
+console.log("🤖 AI_SERVICE_URL:", AI_SERVICE_URL);
 export const searchArtworkAI = async (query, topK = 10) => {
 
   const response = await axios.post(
